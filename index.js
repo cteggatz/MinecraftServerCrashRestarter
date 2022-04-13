debef961f9997f745d4beac75bf23a94290e66f6
cteggatz <categgatzjr@gmail.com>
2022-04-12 21:44:07 -0500

    added some variables so its easy to change things

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const mc = require("minecraft-server-status-simple");
 const spawn = require('child_process').spawn;
 const {execFile} = require("child_process");
 
+//you can edit this variable
+const minecraftServerIP = "UnlondonSeason6.minecraftr.us";
+const mcStartFile = "";
+
+
 
 const restartCheckInterval = 6000;
 
@@ -9,21 +14,18 @@ const restartCheckInterval = 6000;
 let serverStartSwitch = false;
 
 function restartCheck(){
-    mc.status("java", "UnlondonSeason6.minecraftr.us", 25565)
+    mc.status("java", minecraftServerIP, 25565)
     .then((res) => {
         if(res.online == false && serverStartSwitch == false){
             console.log("starting server");
-            /*
-            execFile('test.bat', (error, stdout, stderror) => {
+            execFile(file, (error, stdout, stderror) => {
                 if(error){
                     console.log(`error: ${error.message}`);
                 }
                 if(stderror){
                     console.log('error : ' + stderror)
                 }
-                console.log(stdout)
             })
-            */
             serverStartSwitch = true;
         } 
         if(res.online == true && serverStartSwitch == true){
